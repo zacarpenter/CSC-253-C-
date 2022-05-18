c464ea32ac714b250cef6a9e80aef46ea7ee8b99
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-05-18 14:49:12 -0400

    Add files via upload

---
+++ WPF_HW/WPF_HW/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_HW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        } // end Main
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "3";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "6";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "9";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "0";
+        }
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text = "";
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += " + ";
+        }
+
+        private void btnSub_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += " - ";
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += " * ";
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += " / ";
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay.Text.Contains("*") || txtDisplay.Text.Contains("/"))
+            {
+                // the only issue is you can't multiply 10 * 10 for example
+                // figured it out - use StartsWith instead of Contains
+                if (txtDisplay.Text.StartsWith("0"))
+                {
+                    txtDisplay.Text = "Error";
+                }
+                else
+                {
+                    DataTable calculate = new DataTable();
+                    var equals = calculate.Compute(txtDisplay.Text, "");
+                    txtDisplay.Text += "= " + equals + " ";
+                }
+            }
+            else
+            {
+                DataTable calculate = new DataTable();
+                var equals = calculate.Compute(txtDisplay.Text, "");
+                txtDisplay.Text += "= " + equals + " ";
+            }
+        }
+
+    } // end class
+} // end namespace
---
+++ WeatherLibrary_HW/WeatherLibrary_HW/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLibrary_HW
+{
+    public class WeatherLibraryFuncs
+    {
+        public static double FahrToCel(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5/9;
+        }
+
+        public static double FahrToKelv(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9 + 273.15;
+        }
+
+        public static double CelToFahr(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public static double CelToKelv(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        public static double HeatIndex(double f, double rh)
+        {
+            return -42.379 + (2.04901523 * f) + (10.14333127 * rh) - (0.22475541 * f * rh) - (0.00683783 * f * f) 
+                - (0.5481717 * rh * rh) + (0.00122875 * f * f * rh) + (0.00085282 * f * rh * rh) - (0.00000199 * f * f * rh * rh);
+        }
+
+        public static double WindChill(double s, double f)
+        {
+            return 35.74 + (0.6215 * f) - 35.75 * (Math.Pow(s, 0.16)) + 0.4275 * f * (Math.Pow(s, 0.16));
+        }
+
+        public static double RealtiveHumidity(double s, double a)
+        {
+            return s / a * 100;
+        }
+
+        public static double MphToKnots(double speed)
+        {
+            return speed / 1.151;
+        }
+
+        public static double KnotsToMph(double speed)
+        {
+            return speed * 1.151;
+        }
+
+    } // end class
+} // end namespace
